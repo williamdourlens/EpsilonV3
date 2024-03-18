d7c9637f9880fbe965132cfd9cd48b6735f2a8a5
Hugo Hochart <hugo.hochart@ecoles-epsi.net>
2024-03-18 16:07:28 +0100

    AJout de la page de connexion

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -8,7 +8,15 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class IndexController extends AbstractController
 {
-    #[Route('/', name: 'index')]
+    #[Route('/', name: 'form')]
+    public function form(): Response
+    {
+        return $this->render('pages/Form.html.twig', [
+            'controller_name' => 'FormController',
+        ]);
+    }
+
+    #[Route('/Home', name: 'index')]
     public function index(): Response
     {
         return $this->render('pages/index.html.twig', [
